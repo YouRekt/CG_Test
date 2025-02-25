14ca5e875173b744dce9f1879796c6126d69398d
YouRekt <czarnecki.jurko@gmail.com>
2025-02-25 14:47:40 +0100

    Add project files.

---
+++ Filters/ConvolutionFilters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CG_Test.Filters
+{
+    public static class ConvolutionFilters
+    {
+        public static BitmapSource Blur(BitmapSource source)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static BitmapSource GaussianBlur(BitmapSource source)
+        {
+            throw new NotImplementedException();
+        }
+        public static BitmapSource Sharpen(BitmapSource source)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static BitmapSource EdgeDetection(BitmapSource source)
+        {
+            throw new NotImplementedException();
+        }
+        public static BitmapSource Emboss(BitmapSource source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
